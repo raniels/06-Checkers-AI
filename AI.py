9710d71372f93716fec46bab02ae11502f800e66
raniels <35283647+raniels@users.noreply.github.com>
2018-04-28 22:38:34 -0400

    absolutely nothing

--- AI.py
+++ AI.py
@@ -1,30 +1,30 @@
 #!/usr/bin/python
 class AI:
-	def __init__(self, player):
-		self.player = player
-	
-	def choose_piece(self, board, pieces, opponents):
-		'''
-		By whatever criteria you choose, select a piece to move. You will need to return the piece and its available jumps		
-		'''
-		squares = board.get_squares()
-		for p in pieces:
-			if p.alive:
-				jumps = p.get_valid_possibilities(squares,pieces + opponents)
-				if len(jumps):
-					return (p,jumps)			
-		return (p,[])
-	
-	def move_piece(self, piece,board,pieces,opponents):
-		'''
-		Move the piece to its new location
-		'''
-		squares = board.get_squares()
-		jumps = piece.get_valid_possibilities(squares, pieces+opponents)
-		#move the piece
-		if len(jumps):
-			(row,column) = jumps[0]
-			piece.move(row,column)
-			#get any new jumps (i.e., double-jumps)
-			jumps = piece.check_jump(pieces + opponents,squares)		
-		return (piece,jumps,pieces,opponents)
+    def __init__(self, player):
+        self.player = player
+
+    def choose_piece(self, board, pieces, opponents):
+        '''
+        By whatever criteria you choose, select a piece to move. You will need to return the piece and its available jumps
+        '''
+        squares = board.get_squares()
+        for p in pieces:
+            if p.alive:
+                jumps = p.get_valid_possibilities(squares, pieces + opponents)
+                if len(jumps):
+                    return (p, jumps)
+        return (p, [])
+
+    def move_piece(self, piece, board, pieces, opponents):
+        '''
+        Move the piece to its new location
+        '''
+        squares = board.get_squares()
+        jumps = piece.get_valid_possibilities(squares, pieces + opponents)
+        # move the piece
+        if len(jumps):
+            (row, column) = jumps[0]
+            piece.move(row, column)
+            # get any new jumps (i.e., double-jumps)
+            jumps = piece.check_jump(pieces + opponents, squares)
+        return (piece, jumps, pieces, opponents)
--- checkers.py
+++ checkers.py
@@ -23,166 +23,166 @@ board_highlight = (255,224,102)
 
 pygame.init()
 font = pygame.font.SysFont("arial",30)
-		
+
 #----------------------------------------
 # helper functions
 
 def draw_board(board, alternate, pieces, draw, screen):
-	board.draw(draw,screen,alternate)
-	for p in pieces:
-		p.draw(draw,screen)
-	pygame.display.flip()
+    board.draw(draw,screen,alternate)
+    for p in pieces:
+        p.draw(draw,screen)
+    pygame.display.flip()
 
 def move_piece(square,board,pieces,opponents,all_pieces,pos,selected,jumping,jumps,moves):
-	selected = True
-	if square.highlighted:
-		for p in pieces:
-			if p.alive and p.selected:
-				p.move(square.col,square.row)
-				p.check_king(rows)
-				for j in jumps:
-					if square.position == j['position']:
-						j['piece'].alive = False
-						jumping = p
-						sq = board.get_squares()
-						jumps = jumping.check_jump(all_pieces,sq)
-						if not len(jumps):
-							jumping = None
-				if jumping is None:
-					moves += 1
-	for p in pieces:
-		p.selected = False
-	for s in board.get_squares():
-		s.highlighted = False
-	if jumping is not None:
-		jumping.selected = True
-		for j in jumps:
-			sq = board.get_squares()
-			for s in sq:
-				if j['position'] == s.position:
-					s.highlighted = True
-		selected = True
-	else:
-		selected = False
-		jumps = []
-	return (board,pieces,selected,jumping,jumps,moves)
+    selected = True
+    if square.highlighted:
+        for p in pieces:
+            if p.alive and p.selected:
+                p.move(square.col,square.row)
+                p.check_king(rows)
+                for j in jumps:
+                    if square.position == j['position']:
+                        j['piece'].alive = False
+                        jumping = p
+                        sq = board.get_squares()
+                        jumps = jumping.check_jump(all_pieces,sq)
+                        if not len(jumps):
+                            jumping = None
+                if jumping is None:
+                    moves += 1
+    for p in pieces:
+        p.selected = False
+    for s in board.get_squares():
+        s.highlighted = False
+    if jumping is not None:
+        jumping.selected = True
+        for j in jumps:
+            sq = board.get_squares()
+            for s in sq:
+                if j['position'] == s.position:
+                    s.highlighted = True
+        selected = True
+    else:
+        selected = False
+        jumps = []
+    return (board,pieces,selected,jumping,jumps,moves)
 
 def select_piece(square,board,pieces,all_pieces,pos,jumps):
-	selected = False
-	for p in pieces:
-		if p.alive and p.position == square.position:
-			p.selected = True
-			possibilities = p.get_possibilities(board.get_squares())
-			for h in possibilities:
-				c = board.get_square_coord(h)
-				if c is not None:
-					add = True
-					for a in all_pieces:
-						if a.alive and a.col == c.col and a.row == c.row:
-							add = False
-					if add:
-						c.highlighted = True
-					else:
-						sq = board.get_squares()
-						jumps = p.check_jump(all_pieces,sq)
-						for j in jumps:
-							for s in sq:
-								if j['position'] == s.position:
-									s.highlighted = True
-			selected = True
-	return (board,pieces,selected,jumps)
+    selected = False
+    for p in pieces:
+        if p.alive and p.position == square.position:
+            p.selected = True
+            possibilities = p.get_possibilities(board.get_squares())
+            for h in possibilities:
+                c = board.get_square_coord(h)
+                if c is not None:
+                    add = True
+                    for a in all_pieces:
+                        if a.alive and a.col == c.col and a.row == c.row:
+                            add = False
+                    if add:
+                        c.highlighted = True
+                    else:
+                        sq = board.get_squares()
+                        jumps = p.check_jump(all_pieces,sq)
+                        for j in jumps:
+                            for s in sq:
+                                if j['position'] == s.position:
+                                    s.highlighted = True
+            selected = True
+    return (board,pieces,selected,jumps)
 
 #----------------------------------------
 
 def main():
-	screen = pygame.display.set_mode(size)
-	pygame.display.set_caption("Checkers")
-	clock = pygame.time.Clock()
+    screen = pygame.display.set_mode(size)
+    pygame.display.set_caption("Checkers")
+    clock = pygame.time.Clock()
 
-	moves = 0
-	
-	board = Board(size,constraints,board_colors,board_alternate,board_highlight)
-	
-	# add red pieces to the board
-	red_pieces = []
-	for p in board.red_starting_positions:
-		piece = Piece(p, 'Red', red, board_highlight, board.dim,-1,font)
-		red_pieces.append(piece)
+    moves = 0
 
-	# add black pieces to the board
-	black_pieces = []
-	for p in board.black_starting_positions:
-		piece = Piece(p, 'Black', black, board_highlight, board.dim,1,font)
-		black_pieces.append(piece)
+    board = Board(size,constraints,board_colors,board_alternate,board_highlight)
 
-	all_pieces = red_pieces + black_pieces
+    # add red pieces to the board
+    red_pieces = []
+    for p in board.red_starting_positions:
+        piece = Piece(p, 'Red', red, board_highlight, board.dim,-1,font)
+        red_pieces.append(piece)
 
-	draw_board(board, 0, all_pieces, pygame.draw, screen)
-	
-	selected = False
-	jumping = None
-	jumps = []
-	playing = True
-	winner = ''
-	players = ["Red","Black"]
-	
-	ai = AI('Black')
-	
-	while playing:
-		clock.tick(FPS)
-		currentPlayer = players[moves % len(players)]
+    # add black pieces to the board
+    black_pieces = []
+    for p in board.black_starting_positions:
+        piece = Piece(p, 'Black', black, board_highlight, board.dim,1,font)
+        black_pieces.append(piece)
 
-		if currentPlayer == "Black":
-			pieces = black_pieces
-			opponents = red_pieces
-			(piece,jumps) = ai.choose_piece(board,pieces,opponents)
-			while len(jumps):
-				(piece,jumps,pieces,opponents) = ai.move_piece(piece,board,pieces,opponents)
-				for p in pieces:
-					p.check_king(rows)
-			draw_board(board, moves % len(players), all_pieces, pygame.draw, screen)
-			moves += 1
-			
-		else:
-			pieces = red_pieces
-			opponents = black_pieces
+    all_pieces = red_pieces + black_pieces
 
-			for event in pygame.event.get():
-				if event.type == pygame.QUIT: sys.exit()
-				# handle MOUSEBUTTONUP
-				if event.type == pygame.MOUSEBUTTONUP:
-					pos = pygame.mouse.get_pos()
-					square = board.get_square(pos)
-					if selected:
-						(board,pieces,selected,jumping,jumps,moves) = move_piece(square,board,pieces,opponents,all_pieces,pos,selected,jumping,jumps,moves)
-					else:
-						(board,pieces,selected,jumps) = select_piece(square,board,pieces,all_pieces,pos,jumps)
+    draw_board(board, 0, all_pieces, pygame.draw, screen)
+
+    selected = False
+    jumping = None
+    jumps = []
+    playing = True
+    winner = ''
+    players = ["Red","Black"]
+
+    ai = AI('Black')
+
+    while playing:
+        clock.tick(FPS)
+        currentPlayer = players[moves % len(players)]
+
+        if currentPlayer == "Black":
+            pieces = black_pieces
+            opponents = red_pieces
+            (piece,jumps) = ai.choose_piece(board,pieces,opponents)
+            while len(jumps):
+                (piece,jumps,pieces,opponents) = ai.move_piece(piece,board,pieces,opponents)
+                for p in pieces:
+                    p.check_king(rows)
+            draw_board(board, moves % len(players), all_pieces, pygame.draw, screen)
+            moves += 1
+
+        else:
+            pieces = red_pieces
+            opponents = black_pieces
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT: sys.exit()
+                # handle MOUSEBUTTONUP
+                if event.type == pygame.MOUSEBUTTONUP:
+                    pos = pygame.mouse.get_pos()
+                    square = board.get_square(pos)
+                    if selected:
+                        (board,pieces,selected,jumping,jumps,moves) = move_piece(square,board,pieces,opponents,all_pieces,pos,selected,jumping,jumps,moves)
+                    else:
+                        (board,pieces,selected,jumps) = select_piece(square,board,pieces,all_pieces,pos,jumps)
 
 
-					draw_board(board, moves % len(players), all_pieces, pygame.draw, screen)
+                    draw_board(board, moves % len(players), all_pieces, pygame.draw, screen)
 
-				#check for winning condition
-				red_count = 0
-				black_count = 0
+                #check for winning condition
+                red_count = 0
+                black_count = 0
 
-				for p in all_pieces:
-					if p.alive:
-						if p.player == 'Red':
-							red_count += 1
-						if p.player == 'Black':
-							black_count += 1
+                for p in all_pieces:
+                    if p.alive:
+                        if p.player == 'Red':
+                            red_count += 1
+                        if p.player == 'Black':
+                            black_count += 1
 
-				if red_count == 0:
-					winner = 'Black'
-					playing = False
+                if red_count == 0:
+                    winner = 'Black'
+                    playing = False
 
-				if black_count == 0:
-					winner = 'Red'
-					playing = False
+                if black_count == 0:
+                    winner = 'Red'
+                    playing = False
 
-	
-	print(winner + ' won in only ' + str(moves//2) + ' turns! Good job!')
+
+    print(winner + ' won in only ' + str(moves//2) + ' turns! Good job!')
 
 
 if __name__ == "__main__":
-	main()
+    main()
